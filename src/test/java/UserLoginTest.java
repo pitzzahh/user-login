31eb10d3ec3a8244c5b6ef1c68876fd3ce7442f9
pitzzahh <araopj2191@gmail.com>
2022-05-03 11:07:39 +0800

    refactored checkCredentials() method, fixed bug where user can always log in with wrong credentials.

---
+++ src/test/java/UserLoginTest.java
@@ -0,0 +1,32 @@
+
+import java.io.File;
+import org.junit.jupiter.api.Test;
+import java.io.FileNotFoundException;
+import org.junit.jupiter.api.Assertions;
+
+class UserLoginTest {
+
+    private static final File userNameFile = new File("username.txt");
+    private static final File passwordFile = new File("password.txt");
+
+    @Test
+    void shouldLoginIfCredentialsAreCorrect() throws FileNotFoundException {
+        // given
+        String username = "pitzzahh";
+        String password = "Password123";
+        // when
+        boolean login = UserLogin.checkCredentials(userNameFile, passwordFile, username, password);
+        // then
+        Assertions.assertTrue(login);
+    }
+    @Test
+    void shouldLoginIfCredentialsAreInCorrect() throws FileNotFoundException {
+        // given
+        String username = "pitzzahh";
+        String password = "!Password123";
+        // when
+        boolean login = UserLogin.checkCredentials(userNameFile, passwordFile, username, password);
+        // then
+        Assertions.assertFalse(login);
+    }
+}
