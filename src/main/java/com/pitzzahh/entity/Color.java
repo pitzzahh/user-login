3578f0c6015b1ed7d65eba17418602c4332f55d0
pitzzahh <araopj2191@gmail.com>
2022-05-15 19:44:16 +0800

    quick update

---
+++ src/main/java/com/pitzzahh/entity/Color.java
@@ -0,0 +1,13 @@
+package com.pitzzahh.entity;
+
+/**
+ * class that contains the instance variables that can be used to color text stream.
+ */
+public class Color {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String PURPLE = "\u001B[35m";
+}
